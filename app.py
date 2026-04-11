8042b69a4ed944ed1c3e1390947c31e1f61ad6ff
igorcauandev <igorcauan036@gmail.com>
2026-04-10 21:56:55 -0300

    Instala cors

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from routes.bolsas import bolsas_bp
 from routes.doadores import doadores_bp
 from routes.centrodoacao import homocentro_bp
 from routes.triagem import triagem_bp
 
+CORS(app)
 app = Flask(__name__)
 
 app.register_blueprint(bolsas_bp)
